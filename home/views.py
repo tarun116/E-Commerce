b119ee1d48965c06bc95f99c702ab00e13aa95ce
Tarun <cooltarungupta2@gmail.com>
2021-02-25 14:37:59 +0530

    first commit

---
+++ home/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, HttpResponse
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
+
+def index(request):
+   # return HttpResponse("this is a home page")
+   context ={
+       "variable1" :"I am TARUN"
+   }
+   return render(request,'index.html',context)
+def about(request):
+    return render(request,'about.html')
+    #return HttpResponse("this is a about page")
+def services(request):
+    return render(request,'services.html')
+    #return HttpResponse("this is a services page")
+def contact(request):
+    if request.method == "POST":
+        name=request.POST.get('name')
+        email=request.POST.get('email')
+        phone=request.POST.get('phone')
+        desc=request.POST.get('desc')
+        contact=Contact(name=name, email=email, phone=phone, desc=desc, date=datetime.today())
+        contact.save()
+        messages.success(request, 'Your message has been sent')
+    return render(request,'contact.html')
+    #return HttpResponse("this is a contact page")
